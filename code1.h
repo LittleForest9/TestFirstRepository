928463f1f1f0a67703bc19d609418c5e76eb472a
DESKTOP-8GON3H2\rndpq <rndpqls@gmail.com>
2021-12-04 23:09:26 +0900

    계산기코드(Feat): C++ 계산기 코드 추가
    계산기 덧셈 기능 추가

---
+++ code1.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	float x, y;
+
+	cout << " 두 수를 입력하세요 :";
+	cin >> x >> y;
+	cout << x << " + " << y << " = " << x + y;
+
+	return 0;
+}
